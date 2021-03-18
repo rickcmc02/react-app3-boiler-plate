1eb823da472878ad74b8c4bc5b87cff847303c7d
heeyoung <aa87565115@gmail.com>
2021-03-18 15:24:02 +0900

    protect secret info

--- index.js
+++ index.js
@@ -1,15 +1,37 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+const { User } = require("./models/User");
+
+const config = require('./config/key');
+
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+
+// application/json
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://heeyoung:abcd1234@cluster0.s1s2v.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err))
 
 app.get('/', (req, res) => {
   res.send('Hello World! ~~ hey')
+})
+
+app.post('/register', (req, res) => {
+    // 회원 가입 할 때 필요한 젖ㅇ보들을 client에서 가져오면
+    // 그것들을 데이터베이스에 넣어준다
+    const user = new User(req.body)
+    user.save((err, doc) => {
+        if(err) return res.json({success: false, err})
+        return res.status(200).json({
+            success: true
+        })
+    })
 })
 
 app.listen(port, () => {
